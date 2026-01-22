95842bac5dca81e5b86e62b71484ce236873586a
Fabien Allemand <allemand.fabien@orange.fr>
2026-01-22 15:46:42 +0100

    Improve Athlete page: - Improve stats table - Add gear table

--- src/pages/athlete/callbacks.py
+++ src/pages/athlete/callbacks.py
@@ -3,6 +3,7 @@ This module contains the callbacks of the Athelete page.
 """
 
 import datetime
+import time
 
 import dash_mantine_components as dmc
 from dash import Input, Output, callback
@@ -26,33 +27,133 @@ def create_clubs_string(data):
     )  # TODO improve if clubs list is available
 
 
+def create_activity_totals_rows(data, activity):
+    """
+    Create totals rows for a given activity ("run", "ride", "swim").
+    """
+    return [
+        dmc.TableTr(
+            [
+                dmc.TableTd("Activity Count"),
+                dmc.TableTd(f"{data[f'recent_{activity}_totals']['count']}"),
+                dmc.TableTd(f"{data[f'ytd_{activity}_totals']['count']}"),
+                dmc.TableTd(f"{data[f'all_{activity}_totals']['count']}"),
+            ]
+        ),
+        dmc.TableTr(
+            [
+                dmc.TableTd("Distance"),
+                dmc.TableTd(
+                    f"{data[f'recent_{activity}_totals']['distance'] / 1000:.2f} km"
+                ),
+                dmc.TableTd(
+                    f"{data[f'ytd_{activity}_totals']['distance'] / 1000:.2f} km"
+                ),
+                dmc.TableTd(
+                    f"{data[f'all_{activity}_totals']['distance'] / 1000:.2f} km"
+                ),
+            ]
+        ),
+        dmc.TableTr(
+            [
+                dmc.TableTd(
+                    "Moving / Elapsed Time",
+                ),
+                dmc.TableTd(
+                    time.strftime(
+                        "%H:%M:%S",
+                        time.gmtime(data[f"recent_{activity}_totals"]["moving_time"]),
+                    )
+                    + " / "
+                    + time.strftime(
+                        "%H:%M:%S",
+                        time.gmtime(data[f"recent_{activity}_totals"]["elapsed_time"]),
+                    )
+                ),
+                dmc.TableTd(
+                    time.strftime(
+                        "%H:%M:%S",
+                        time.gmtime(data[f"ytd_{activity}_totals"]["moving_time"]),
+                    )
+                    + " / "
+                    + time.strftime(
+                        "%H:%M:%S",
+                        time.gmtime(data[f"ytd_{activity}_totals"]["elapsed_time"]),
+                    )
+                ),
+                dmc.TableTd(
+                    time.strftime(
+                        "%H:%M:%S",
+                        time.gmtime(data[f"all_{activity}_totals"]["moving_time"]),
+                    )
+                    + " / "
+                    + time.strftime(
+                        "%H:%M:%S",
+                        time.gmtime(data[f"all_{activity}_totals"]["elapsed_time"]),
+                    )
+                ),
+            ]
+        ),
+        dmc.TableTr(
+            [
+                dmc.TableTd("Elevation Gain"),
+                dmc.TableTd(
+                    f"{data[f'recent_{activity}_totals']['elevation_gain']:.2f} m"
+                ),
+                dmc.TableTd(
+                    f"{data[f'ytd_{activity}_totals']['elevation_gain']:.2f} m"
+                ),
+                dmc.TableTd(
+                    f"{data[f'all_{activity}_totals']['elevation_gain']:.2f} m"
+                ),
+            ]
+        ),
+    ]
+
+
 def create_totals_table(data):
     """
-    Create totals stats table.
+    Create table of totals.
     """
     return dmc.Table(
-        data={
-            "body": [
+        children=[
+            # Table head
+            dmc.TableThead(
+                dmc.TableTr(
+                    [
+                        dmc.TableTh(""),
+                        dmc.TableTh("Recent"),
+                        dmc.TableTh(f"{datetime.datetime.now().date().year}"),
+                        dmc.TableTh("All Time"),
+                    ]
+                )
+            ),
+            # Table body
+            dmc.TableTbody(
                 [
-                    "Activity Count",
-                    f"{data['count']}",
-                ],
-                [
-                    "Distance",
-                    f"{data['distance'] / 1000:.2f} km",
-                ],
-                [
-                    "Elapsed / Moving Time",
-                    f"{data['elapsed_time']:.2f} / {data['moving_time']:.2f}",
-                ],
-                [
-                    "Elevation Gain",
-                    f"{data['elevation_gain']:.2f} m",
-                ],
-            ],
-        },
+                    dmc.TableTr(
+                        dmc.TableTd(
+                            "Running", bg="lightgray", tableProps={"colSpan": 4}
+                        )
+                    ),
+                    *create_activity_totals_rows(data, "run"),
+                    dmc.TableTr(
+                        dmc.TableTd(
+                            "Cycling", bg="lightgray", tableProps={"colSpan": 4}
+                        )
+                    ),
+                    *create_activity_totals_rows(data, "ride"),
+                    dmc.TableTr(
+                        dmc.TableTd(
+                            "Swimming", bg="lightgray", tableProps={"colSpan": 4}
+                        )
+                    ),
+                    *create_activity_totals_rows(data, "swim"),
+                ]
+            ),
+        ],
         striped=False,
-        highlightOnHover=False,
+        highlightOnHover=True,
         withTableBorder=False,
         withColumnBorders=False,
     )
@@ -100,27 +201,94 @@ def register_callbacks():
     )
     def update_stats_card(_, data):
         """
-        Update the athlete card.
+        Update the stats card.
         """
-        return dmc.Group(
+        return dmc.Stack(
             [
-                dmc.Stack(
-                    [
-                        dmc.Title(
-                            f"{datetime.datetime.now().date().year} Totals", order=1
+                dmc.Title("Totals", order=1),
+                create_totals_table(data),
+            ]
+        )
+
+    @callback(
+        Output({"page": "athlete", "component": "gear-card"}, "children"),
+        [
+            Input("url", "pathname"),
+            Input("athlete-store", "data"),
+        ],
+    )
+    def update_gear_card(_, data):
+        """
+        Update the gear card.
+        """
+        return dmc.Stack(
+            [
+                dmc.Title("Gear", order=1),
+                dmc.Table(
+                    children=[
+                        # Table head
+                        dmc.TableThead(
+                            dmc.TableTr(
+                                [
+                                    dmc.TableTh("Name"),
+                                    dmc.TableTh("Distance"),
+                                ]
+                            )
                         ),
-                        create_totals_table(data["ytd_run_totals"]),
-                        create_totals_table(data["ytd_ride_totals"]),
-                        create_totals_table(data["ytd_swim_totals"]),
-                    ]
-                ),
-                dmc.Stack(
-                    [
-                        dmc.Title("Totals", order=1),
-                        create_totals_table(data["all_run_totals"]),
-                        create_totals_table(data["all_ride_totals"]),
-                        create_totals_table(data["all_swim_totals"]),
-                    ]
+                        # Table body
+                        dmc.TableTbody(
+                            [
+                                dmc.TableTr(
+                                    dmc.TableTd(
+                                        "Shoes",
+                                        bg="lightgray",
+                                        tableProps={"colSpan": 2},
+                                    )
+                                ),
+                                *[
+                                    dmc.TableTr(
+                                        [
+                                            dmc.TableTd(shoe["name"]),
+                                            dmc.TableTd(
+                                                f"{shoe['distance'] / 1000:.2f} km"
+                                            ),
+                                        ]
+                                    )
+                                    for shoe in sorted(
+                                        data["shoes"],
+                                        reverse=True,
+                                        key=lambda item: item["distance"],
+                                    )
+                                ],
+                                dmc.TableTr(
+                                    dmc.TableTd(
+                                        "Bikes",
+                                        bg="lightgray",
+                                        tableProps={"colSpan": 2},
+                                    )
+                                ),
+                                *[
+                                    dmc.TableTr(
+                                        [
+                                            dmc.TableTd(bike["name"]),
+                                            dmc.TableTd(
+                                                f"{bike['distance'] / 1000:.2f} km"
+                                            ),
+                                        ]
+                                    )
+                                    for bike in sorted(
+                                        data["bikes"],
+                                        reverse=True,
+                                        key=lambda item: item["distance"],
+                                    )
+                                ],
+                            ]
+                        ),
+                    ],
+                    striped=False,
+                    highlightOnHover=True,
+                    withTableBorder=False,
+                    withColumnBorders=False,
                 ),
             ]
         )
