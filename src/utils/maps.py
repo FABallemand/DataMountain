5d04628c6d1f5fed5cef98a0c61551ee670ddbd0
Fabien Allemand <allemand.fabien@orange.fr>
2026-01-31 12:14:42 +0100

    Improve Map page: - Improve centering (do not account for outliers) - Add dynamic zoom (do not account for outliers)

--- src/utils/maps.py
+++ src/utils/maps.py
@@ -2,13 +2,79 @@
 This module contains the utilities for maps visualisation.
 """
 
+from math import log2
+
 import plotly.graph_objects as go
+import polars as pl
 import polyline
+
+
+def compute_map_coords(
+    center_lats: list,
+    center_lons: list,
+    min_lats: list,
+    min_lons: list,
+    max_lats: list,
+    max_lons: list,
+    threshold: float = 1.5,
+) -> dict[str, float]:
+    """
+    Compute map reference coordinates (without outliers) for map
+    plotting (i.e.: min, max and center coordinates).
+
+    Args:
+        center_lats (list): List of center latitudes.
+        center_lons (list): List of center longitudes.
+        min_lats (list): List of minimum latitudes.
+        min_lons (list): List of minimum longitudes.
+        max_lats (list): List of maximum latitudes.
+        max_lons (list): List of maximum longitudes.
+        threshold (float, optional): Z-score threshold used to remove
+            outliers. Defaults to 1.5.
+
+    Returns:
+        dict[str, float]: Dictionary of reference coordinates for map
+            plotting.
+    """
+    df = (
+        pl.DataFrame(
+            {
+                "center_lats": center_lats,
+                "center_lons": center_lons,
+                "min_lats": min_lats,
+                "min_lons": min_lons,
+                "max_lats": max_lats,
+                "max_lons": max_lons,
+            }
+        )
+        .with_columns(
+            (
+                (pl.col("center_lats") - pl.col("center_lats").mean())
+                / pl.col("center_lats").std()
+            ).alias("z_center_lats"),
+            (
+                (pl.col("center_lons") - pl.col("center_lons").mean())
+                / pl.col("center_lons").std()
+            ).alias("z_center_lons"),
+        )
+        .filter(
+            (pl.col("z_center_lats") < threshold)
+            & (pl.col("z_center_lons") < threshold)
+        )
+    )
+    return {
+        "center_lat": df.get_column("center_lats").mean(),
+        "center_lon": df.get_column("center_lons").mean(),
+        "min_lat": df.get_column("min_lats").min(),
+        "min_lon": df.get_column("min_lons").min(),
+        "max_lat": df.get_column("max_lats").max(),
+        "max_lon": df.get_column("max_lons").max(),
+    }
 
 
 def _create_scattermap(
     polyline_str: str, name: str = "", color: str = "#FFA800"
-) -> tuple[go.Scattermap, float, float]:
+) -> tuple[go.Scattermap, dict[str, float]]:
     """
     Create a Scattermap object from a polyline string and return it
     along with the center coordinates.
@@ -19,7 +85,9 @@ def _create_scattermap(
         color (str, optional): Color of the trace. Defaults to "#FFA800".
 
     Returns:
-        tuple[go.Scattermap, float, float]: Scattermap object and center coordinates.
+        tuple[go.Scattermap, dict[str, float]]: Scattermap object and
+            dictionary containting useful coordinates (min, max and
+            center latitude and longitude).
     """
     decoded_polyline = polyline.decode(polyline_str)
 
@@ -41,8 +109,14 @@ def _create_scattermap(
                 "color": color,
             },
         ),
-        center_lat,
-        center_lon,
+        {
+            "center_lat": center_lat,
+            "center_lon": center_lon,
+            "min_lat": min_lat,
+            "min_lon": min_lon,
+            "max_lat": max_lat,
+            "max_lon": max_lon,
+        },
     )
 
 
@@ -76,21 +150,38 @@ def create_map(
         color = [color]
 
     fig = go.Figure()
-    center_lat, center_lon = 0, 0
+
+    # Iterate over polylines
+    center_lats, center_lons = [], []
+    min_lats, min_lons, max_lats, max_lons = [], [], [], []
     for pl_str, n, c in zip(polyline_str, name, color):
-        scattermap, lat, lon = _create_scattermap(pl_str, n, c)
+        scattermap, coords = _create_scattermap(pl_str, n, c)
         fig.add_trace(scattermap)
-        center_lat += lat
-        center_lon += lon
+        center_lats.append(coords["center_lat"])
+        center_lons.append(coords["center_lon"])
+        min_lats.append(coords["min_lat"])
+        min_lons.append(coords["min_lon"])
+        max_lats.append(coords["max_lat"])
+        max_lons.append(coords["max_lon"])
+    # Remove outliers from coordinates
+    map_coords = compute_map_coords(
+        center_lats, center_lons, min_lats, min_lons, max_lats, max_lons
+    )
+    # Compute map zoom
+    lat_range = map_coords["max_lat"] - map_coords["min_lat"]
+    lon_range = map_coords["max_lon"] - map_coords["min_lon"]
+    max_range = max(lat_range, lon_range)
+    zoom = 8 - log2(max_range + 1e-6)
+    # Update figure layout
     fig.update_layout(
         margin={"l": 0, "t": 0, "b": 0, "r": 0},
         map={
             "center": {
-                "lon": center_lon / len(polyline_str),
-                "lat": center_lat / len(polyline_str),
+                "lon": map_coords["center_lon"],
+                "lat": map_coords["center_lat"],
             },
             "style": map_layer,
-            "zoom": 10,
+            "zoom": zoom,
         },
     )
     return fig
